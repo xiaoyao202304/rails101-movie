b0382425db0013209ca4bcca8ab8f35b7f0ce41e
yaoyao <yao_zhou@icloud.com>
2017-03-23 22:14:48 -0700

    implement movies CRUD

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,50 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path, notice: "新增电影成功"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "已更新"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, alert: "已删除"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :image, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :groups do
-    member do
-      post :join
-      post :quit
-    end
+  resources :movies
 
-    resources :posts
-  end
+
 
   namespace :account do
     resources :groups
     resources :posts
   end
 
-  root "groups#index"
+  root "movies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
